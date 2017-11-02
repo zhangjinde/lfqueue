bb019b2b227581f36b674e532d4b7a69454c2b79
kautism <darkautism@gmail.com>
2017-11-02 20:48:10 +0800

    Fix visual stdio check windows define

--- test_multithread.c
+++ test_multithread.c
@@ -2,18 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#if defined (__WIN32__)
+#if ( defined (__WIN32__) || defined(_WIN32) )
 #include <windows.h>
 #else
 #include <pthread.h>
+#include <unistd.h>
 #endif
-
-
-
 
 #include <stdint.h>
 #include <inttypes.h>
-#include <unistd.h>
 #include <time.h>
 #include "lfq.h"
 
@@ -34,7 +31,7 @@ struct user_data{
 	long data;
 };
 
-#if defined (__WIN32__)
+#if ( defined (__WIN32__) || defined(_WIN32) )
 DWORD WINAPI 
 #else
 void * 
@@ -59,7 +56,7 @@ addq( void * data ) {
 	return 0;
 }
 
-#if defined (__WIN32__)
+#if ( defined (__WIN32__) || defined(_WIN32) )
 DWORD WINAPI 
 #else
 void * 
@@ -76,7 +73,7 @@ delq( void * data ) {
 			free(p);
 			__sync_add_and_fetch(&cn_deled, 1);			
 		} else
-#if defined (__WIN32__)
+#if ( defined (__WIN32__) || defined(_WIN32) )
 			SwitchToThread();
 #else
 			pthread_yield(); // queue is empty, release CPU slice
@@ -92,7 +89,7 @@ int main() {
 	struct lfq_ctx ctx;
 	int i=0;
 	lfq_init(&ctx);
-#if defined (__WIN32__)
+#if ( defined (__WIN32__) || defined(_WIN32) )
 	HANDLE thread_d[MAX_CONSUMER];
 	HANDLE thread_a[MAX_PRODUCER];
 #else
@@ -102,7 +99,7 @@ int main() {
 	
 	__sync_add_and_fetch(&cn_producer, 1);
 	for ( i = 0 ; i < MAX_CONSUMER ; i++ )
-#if defined (__WIN32__)
+#if ( defined (__WIN32__) || defined(_WIN32) )
 		thread_d[i] = CreateThread(NULL, 0, delq, &ctx, 0, 0); 
 #else
 		pthread_create(&thread_d[i], NULL , delq , (void*) &ctx);
@@ -110,7 +107,7 @@ int main() {
 	
 	for ( i = 0 ; i < MAX_PRODUCER ; i++ ){
 		__sync_add_and_fetch(&cn_producer, 1);
-#if defined (__WIN32__)
+#if ( defined (__WIN32__) || defined(_WIN32) )
 		thread_a[i] = CreateThread(NULL, 0, addq, &ctx, 0, 0); 
 #else
 		pthread_create(&thread_a[i], NULL , addq , (void*) &ctx);
@@ -119,7 +116,7 @@ int main() {
 	
 	__sync_sub_and_fetch(&cn_producer, 1);
 	
-#if defined (__WIN32__)
+#if ( defined (__WIN32__) || defined(_WIN32) )
 	WaitForMultipleObjects(MAX_PRODUCER, thread_a, 1, INFINITE);
 	WaitForMultipleObjects(MAX_CONSUMER, thread_d, 1, INFINITE);
 #else
