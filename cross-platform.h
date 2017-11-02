165f816891e05fcf18eb304babe3da41e8ab4257
kautism <darkautism@gmail.com>
2017-11-03 00:38:31 +0800

    Try to add atomic version

--- cross-platform.h
+++ cross-platform.h
@@ -1,48 +1,43 @@
 #ifndef __CROSS_PLATFORM_H__
 #define __CROSS_PLATFORM_H__
-// bool define
-#ifdef __KERNEL__
-	#include <sys/stdbool.h>
-#else
-	#include <stdbool.h>
-#endif
-
-// malloc free
 #ifdef __KERNEL__
 	#define malloc(x) kmalloc(x, GFP_KERNEL )
 	#define free kfree
 	#define calloc(x,y) kmalloc(x*y, GFP_KERNEL | __GFP_ZERO )
 	#include<linux/string.h>
+	#include <sys/stdbool.h>
 #else
 	#include <stdlib.h>
 	#include <string.h>
+	#include <stdbool.h>
 #endif
 
 #ifndef asm
 	#define asm __asm
 #endif
 
-#define cmpxchg( ptr, _old, _new ) {						\
-  volatile uint32_t *__ptr = (volatile uint32_t *)(ptr);	\
-  uint32_t __ret;                                   		\
-  asm volatile( "lock; cmpxchgl %2,%1"          			\
-    : "=a" (__ret), "+m" (*__ptr)               			\
-    : "r" (_new), "0" (_old)                    			\
-    : "memory");                							\
-  );                                            			\
-  __ret;                                        			\
-}
+#ifndef __GCC__
+	#include <windows.h>
+	#include <stdatomic.h>
+	#define CAS atomic_compare_exchange_weak
+	#define ATOMIC_ADD atomic_fetch_add
+	#define ATOMIC_SUB atomic_fetch_sub
+	#define ATOMIC_SET atomic_store
+	#define mb() atomic_thread_fence(memory_order_acq_rel)
+	#define smb() atomic_thread_fence(memory_order_release)
+	#define lmb() atomic_thread_fence(memory_order_acquire)
+#else
+	#define CAS __sync_bool_compare_and_swap
+	#define ATOMIC_ADD __sync_add_and_fetch
+	#define ATOMIC_SUB __sync_sub_and_fetch
+	#define ATOMIC_SET __sync_lock_test_and_set
+	#define mb __sync_synchronize
+	#define smb() asm volatile( "sfence" )
+	#define lmb() asm volatile( "lfence" )
+#endif
 
-//#define CAS cmpxchg
-#define CAS __sync_bool_compare_and_swap
-#define ATOMIC_ADD __sync_add_and_fetch
-#define ATOMIC_SUB __sync_sub_and_fetch
-#define ATOMIC_SET __sync_lock_test_and_set
-#define ATOMIC_RELEASE __sync_lock_release
-#define mb __sync_synchronize
 
-#define lmb() asm volatile( "lfence" )
-#define smb() asm volatile( "sfence" )
+
 
 #endif
 
